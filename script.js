ced81ae679143c83a6a93b1f00eab3cfb4e68f30
miamikyu <26570204@students.lincoln.ac.uk>
2023-08-20 14:41:46 +0100

    need to add swap convert code

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+document.querySelector(".text").style.display="block";
+function swapConvert(){
+    if(document.getElementById("input").placeholder==="°C"){
+        document.getElementsById("input")[0].placeholder="°F";
+    }
+    else{
+        document.getElementsById("input")[0].placeholder="°C"; 
+    }
+    
+}
+function convert(){
+    if(document.getElementById("input").value===''){
+        alert("Please write something!");
+    }
+    else{
+        var cTemp = parseFloat(document.getElementById("input").value);
+        var fTemp = (cTemp*(9/5))+32;
+        document.querySelector(".text").innerHTML=fTemp+"°F";
+    }
+}

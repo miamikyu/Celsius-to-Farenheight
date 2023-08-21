2a6b892afb6fcf46489fdcf7972f67e5ea1f9bca
miamikyu <26570204@students.lincoln.ac.uk>
2023-08-21 13:54:43 +0100

    all finished

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 document.querySelector(".text").style.display="block";
 function swapConvert(){
-    if(document.getElementById("input").placeholder==="°C"){
-        document.getElementsById("input")[0].placeholder="°F";
+    document.getElementById("input").value="";
+    if(document.getElementById("input").placeholder=="°C"){
+        document.getElementById("input").placeholder="°F";
     }
-    else{
-        document.getElementsById("input")[0].placeholder="°C"; 
+    else if(document.getElementById("input").placeholder=="°F"){
+        document.getElementById("input").placeholder="°C";
     }
-    
 }
+
+
 function convert(){
-    if(document.getElementById("input").value===''){
+    if(document.getElementById("input").value===""){
         alert("Please write something!");
     }
-    else{
+    else if(document.getElementById("input").placeholder=="°C"){
         var cTemp = parseFloat(document.getElementById("input").value);
         var fTemp = (cTemp*(9/5))+32;
         document.querySelector(".text").innerHTML=fTemp+"°F";
+    }
+    else{
+        var fTemp=parseFloat(document.getElementById("input").value);
+        var cTemp =((fTemp-32)*(5/9));
+        document.querySelector(".text").innerHTML=cTemp+"°C";
     }
 }
